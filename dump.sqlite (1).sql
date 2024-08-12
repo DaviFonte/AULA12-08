47f98740b9fbe683504881400b369ed21b5cb1a1
DaviFonte <159053797+DaviFonte@users.noreply.github.com>
2024-08-12 13:54:07 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,24 @@
+-- TABLE
+CREATE TABLE CAMPO(
+   ID INT(4),
+   CIDADE VARCHAR(30) UNIQUE,
+   CAPACIDADE INT(4) NOT NULL
+  );
+CREATE TABLE jogador(
+  ID int(4),
+  NOME VARCHAR(30),
+  ESPORTE VARCHAR(30)
+  
+  
+  );
+CREATE TABLE sala(
+mesa varchar(50),
+pc varchar(10)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
